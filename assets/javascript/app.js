0484a45c552cdbfaf73b460ce78d833b0fc90a89
Mark Langhamer <langhamerm@gmail.com>
2018-11-17 11:26:29 -0600

    Cloudinary Upload Functionality

---
+++ assets/javascript/app.js
@@ -0,0 +1,7 @@
+//Set Cloudinary cloud name
+cloudinary.setCloudName('djzrsujql');
+//Cloudinary Upload Widget Implementation
+document.getElementById("upload_widget_opener").addEventListener("click", function () {
+    cloudinary.openUploadWidget({ cloud_name: 'djzrsujql', upload_preset: 'pa9uueiy', cropping: 'server', folder: 'MeetMeHalfway'},
+        function (error, result) { console.log(error, result) });
+}, false);
