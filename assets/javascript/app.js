68f39ccf82ed412ebfd1238ba9b8b42512cb0d9c
Mark Langhamer <langhamerm@gmail.com>
2018-11-19 20:06:23 -0600

    find meeting locations functionality added

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -40,3 +40,137 @@ $("#submit-chat").on("click", function (event) {
 database.ref("/chat").orderByChild("dateAdded").limitToLast(1).on("value", function (snapshot) {
     $("#chat-window").append("</br>" + snapshot.val().message + "</br>");
 });
+// Maps stuff
+var map, infoWindow;
+var latVar = 41.85;
+var lngVar = -87.64;
+
+function initMap() {
+    map = new google.maps.Map(document.getElementById("map"), { center: { lat: latVar, lng: lngVar }, zoom: 13 });
+    infoWindow = new google.maps.InfoWindow;
+    var evanston = { lat: 42.0577401, lng: -87.67564449999999 };
+    // Try HTML5 geoloc
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function (position) {
+            var pos = {
+                lat: position.coords.latitude,
+                lng: position.coords.longitude
+            };
+
+            infoWindow.setPosition(pos);
+            infoWindow.setContent("Location found.");
+            infoWindow.open(map);
+            map.setCenter(pos);
+            //Click to search restaurants nearby
+            var restaurantButton = document.getElementById('find-food');
+            restaurantButton.onclick = function () {
+                service.nearbySearch(
+                    { location: pos, radius: 5000, type: ['restaurant'] },
+                    function (results, status, pagination) {
+                        if (status !== 'OK') return;
+
+                        createMarkers(results);
+                        moreButton.disabled = !pagination.hasNextPage;
+                        getNextPage = pagination.hasNextPage && function () {
+                            pagination.nextPage();
+                        };
+                    });
+            };
+            //Click to search bars nearby 
+            var barButton = document.getElementById('find-bars');
+            barButton.onclick = function () {
+                service.nearbySearch(
+                    { location: pos, radius: 5000, type: ['bar'] },
+                    function (results, status, pagination) {
+                        if (status !== 'OK') return;
+
+                        createMarkers(results);
+                        moreButton.disabled = !pagination.hasNextPage;
+                        getNextPage = pagination.hasNextPage && function () {
+                            pagination.nextPage();
+                        };
+                    });
+            };
+            //Click to search gyms nearby
+            var gymButton = document.getElementById('find-gyms');
+            gymButton.onclick = function () {
+                service.nearbySearch(
+                    { location: pos, radius: 5000, type: ['gym'] },
+                    function (results, status, pagination) {
+                        if (status !== 'OK') return;
+
+                        createMarkers(results);
+                        moreButton.disabled = !pagination.hasNextPage;
+                        getNextPage = pagination.hasNextPage && function () {
+                            pagination.nextPage();
+                        };
+                    });
+            };
+            //Click to search parks nearby
+            var parkButton = document.getElementById('find-parks');
+            parkButton.onclick = function () {
+                service.nearbySearch(
+                    { location: pos, radius: 5000, type: ['park'] },
+                    function (results, status, pagination) {
+                        if (status !== 'OK') return;
+
+                        createMarkers(results);
+                        moreButton.disabled = !pagination.hasNextPage;
+                        getNextPage = pagination.hasNextPage && function () {
+                            pagination.nextPage();
+                        };
+                    });
+            };
+        }, function () {
+            handleLocationError(true, infoWindow, map.getCenter());
+        });
+    } else {
+        handleLocationError(false, infoWindow, map.getCenter());
+    }
+    // Create the places service.
+    var service = new google.maps.places.PlacesService(map);
+    var getNextPage = null;
+    var moreButton = document.getElementById('more');
+    moreButton.onclick = function () {
+        moreButton.disabled = true;
+        if (getNextPage) getNextPage();
+    };
+};
+
+
+function createMarkers(places) {
+    var bounds = new google.maps.LatLngBounds();
+    var placesList = document.getElementById('places');
+
+    for (var i = 0, place; place = places[i]; i++) {
+        var image = {
+            url: place.icon,
+            size: new google.maps.Size(71, 71),
+            origin: new google.maps.Point(0, 0),
+            anchor: new google.maps.Point(17, 34),
+            scaledSize: new google.maps.Size(25, 25)
+        };
+
+        var marker = new google.maps.Marker({
+            map: map,
+            icon: image,
+            title: place.name,
+            position: place.geometry.location
+        });
+
+        var li = document.createElement('li');
+        li.textContent = place.name;
+        placesList.appendChild(li);
+
+        bounds.extend(place.geometry.location);
+    }
+    map.fitBounds(bounds);
+}
+
+
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+    infoWindow.setPosition(pos);
+    infoWindow.setContent(browserHasGeolocation ? "Error: The Geolocation service failed" : "Error: Your browser doesn\'t support geolocation");
+    infoWindow.open(map);
+};
+
