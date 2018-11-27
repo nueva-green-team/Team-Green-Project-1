683a12f46887d6b1625bb18db9d5a6bb87bf77b9
Mark Langhamer <langhamerm@gmail.com>
2018-11-26 20:07:24 -0600

    Mark push

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,22 +2,22 @@
 cloudinary.setCloudName('djzrsujql');
 //Cloudinary Upload Widget Implementation
 document.getElementById("upload_widget_opener").addEventListener("click", function () {
-    cloudinary.openUploadWidget({ cloud_name: 'djzrsujql', upload_preset: 'pa9uueiy', cropping: 'server', folder: 'MeetMeHalfway' },
-        function (error, result) { console.log(error, result) });
+  cloudinary.openUploadWidget({ cloud_name: 'djzrsujql', upload_preset: 'pa9uueiy', cropping: 'server', folder: 'MeetMeHalfway' },
+    function (error, result) { console.log(error, result) });
 }, false);
 
 // for the image grid layout
 function getRandomSize(min, max) {
-    return Math.round(Math.random() * (max - min) + min);
-  }
-  
-  var allImages = "";
-  
-  for (var i = 0; i < 25; i++) {
-    var width = getRandomSize(200, 400);
-    var height =  getRandomSize(200, 400);
-    allImages += '<img src="https://placekitten.com/'+width+'/'+height+'" alt="pretty kitty">';
-  }
-  
-  $('#photos').append(allImages);
-  $("#profile-pic").html()
+  return Math.round(Math.random() * (max - min) + min);
+}
+
+var allImages = "";
+
+for (var i = 0; i < 25; i++) {
+  var width = getRandomSize(200, 400);
+  var height = getRandomSize(200, 400);
+  allImages += '<img src="https://placekitten.com/' + width + '/' + height + '" alt="pretty kitty">';
+}
+
+$('#photos').append(allImages);
+$("#profile-pic").html();
