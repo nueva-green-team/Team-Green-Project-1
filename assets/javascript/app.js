762aa2aa6648b2d36e96269bda821503a767beba
Mark Langhamer <langhamerm@gmail.com>
2018-11-27 01:47:59 -0600

    add console logs

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -94,6 +94,7 @@ firebase.auth().signInWithPopup(provider).then(function(result) {
     var token = result.credential.accessToken;
     // The signed-in user info.
     var user = result.user;
+    console.log(result);
     // ...
   }).catch(function(error) {
     // Handle Errors here.
@@ -114,6 +115,7 @@ firebase.auth().signInWithPopup(provider).then(function(result) {
     }
     // The signed-in user info.
     var user = result.user;
+    console.log(result);
   }).catch(function(error) {
     // Handle Errors here.
     var errorCode = error.code;
