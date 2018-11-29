2a7a377fe02c37d2144fec835286959cc5f7d506
Mark Langhamer <langhamerm@gmail.com>
2018-11-28 22:02:02 -0600

    s

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -309,7 +309,7 @@ $("#like-btn").on("click", function () {
 });
 //Photo Collage
 database.ref("/players").once("value").then(function(snapshot){
-$("#submit-user-password-btn").on("click", function () {
+$("#btn").on("click", function () {
 
   console.log(snapshot);
   for (var i = 0; i < response.length; i++) {
