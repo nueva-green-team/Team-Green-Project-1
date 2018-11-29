3480b82c846257ad430acbeb9c5538d244064c6d
Mark Langhamer <langhamerm@gmail.com>
2018-11-29 02:19:18 -0600

    s

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -367,3 +367,80 @@ $("#btn").on("click", function () {
 //           }
 //         });
 // }
+var Geocoder = new google.maps.Geocoder;
+
+function geocodeLatLng(latlng, cb) {
+  return Geocoder.geocode({
+    'location': latlng
+  }, function(res, status) {
+    return cb((status === 'OK') ? (res[1] || "Nothing found") : status);
+  });
+}
+
+
+// The ones you get from weather
+var coordinates = {
+  lat: 45.8150,
+  lng: 15.9819
+}
+
+geocodeLatLng(coordinates, function(data) {
+  console.log(data.address_components[0].long_name); // City
+  console.dir(data.address_components[2].long_name); // Country
+});
+var geocoder;
+
+if (navigator.geolocation) {
+  navigator.geolocation.getCurrentPosition(successFunction, errorFunction);
+} 
+//Get the latitude and the longitude;
+function successFunction(position) {
+  var lat = position.coords.latitude;
+  var lng = position.coords.longitude;
+  codeLatLng(lat, lng)
+}
+
+function errorFunction(){
+  alert("Geocoder failed");
+}
+
+function initialize() {
+  geocoder = new google.maps.Geocoder();
+
+
+
+}
+
+function codeLatLng(lat, lng) {
+
+  var latlng = new google.maps.LatLng(lat, lng);
+  geocoder.geocode({'latLng': latlng}, function(results, status) {
+    if (status == google.maps.GeocoderStatus.OK) {
+    console.log(results)
+      if (results[1]) {
+       //formatted address
+       alert(results[0].formatted_address)
+      //find country name
+           for (var i=0; i<results[0].address_components.length; i++) {
+          for (var b=0;b<results[0].address_components[i].types.length;b++) {
+
+          //there are different types that might hold a city admin_area_lvl_1 usually does in come cases looking for sublocality type will be more appropriate
+              if (results[0].address_components[i].types[b] == "administrative_area_level_1") {
+                  //this is the object you are looking for
+                  city= results[0].address_components[i];
+                  break;
+              }
+          }
+      }
+      //city data
+      alert(city.short_name + " " + city.long_name)
+
+
+      } else {
+        alert("No results found");
+      }
+    } else {
+      alert("Geocoder failed due to: " + status);
+    }
+  });
+};
