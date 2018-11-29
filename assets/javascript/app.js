4372d7c24995e20e194fdef1ae0718c71e9c7de8
Mark Langhamer <langhamerm@gmail.com>
2018-11-28 18:01:37 -0600

    Moved scripts

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,45 +1,98 @@
-//Set Cloudinary cloud name
-cloudinary.setCloudName('djzrsujql');
-//Cloudinary Upload Widget Implementation
-document.getElementById("upload_widget_opener").addEventListener("click", function () {
-  cloudinary.openUploadWidget({ cloud_name: 'djzrsujql', upload_preset: 'pa9uueiy', cropping: 'server', folder: 'MeetMeHalfway' },
-    function (error, result) { console.log(error, result) });
-}, false);
-
-// for the image grid layout
-// function getRandomSize(min, max) {
-//     return Math.round(Math.random() * (max - min) + min);
-//   }
-
-//   var allImages = "";
-
-//   for (var i = 0; i < 25; i++) {
-//     var width = getRandomSize(200, 400);
-//     var height =  getRandomSize(200, 400);
-//     allImages += '<img src="https://placekitten.com/'+width+'/'+height+'" alt="pretty kitty">';
-//   }
-
-//   $('#photos').append(allImages);
-function getRandomSize(min, max) {
-  return Math.round(Math.random() * (max - min) + min);
+//Facebook
+// This is called with the results from from FB.getLoginStatus().
+function statusChangeCallback(response) {
+  console.log('statusChangeCallback');
+  console.log(response);
+  // The response object is returned with a status field that lets the
+  // app know the current login status of the person.
+  // Full docs on the response object can be found in the documentation
+  // for FB.getLoginStatus().
+  if (response.status === 'connected') {
+    // Logged into your app and Facebook.
+    testAPI();
+    profilePic();
+  } else {
+    // The person is not logged into your app or we are unable to tell.
+    document.getElementById('status').innerHTML = 'Please log ' +
+      'into this app.';
+  }
 }
 
-var allImages = "";
-
-for (var i = 0; i < 25; i++) {
-  var width = getRandomSize(200, 400);
-  var height = getRandomSize(200, 400);
-  allImages += '<img src="https://placekitten.com/' + width + '/' + height + '" alt="pretty kitty">';
+// This function is called when someone finishes with the Login
+// Button.  See the onlogin handler attached to it in the sample
+// code below.
+function checkLoginState() {
+  FB.getLoginStatus(function (response) {
+    statusChangeCallback(response);
+  });
 }
-$('#photos').append(allImages);
-// added cloudinary photos with angle//
-cloudinary.imageTag('front_face.png', {
-  secure: true, transformation: [
-    { width: 150, height: 150, gravity: "face", radius: 20, effect: "sepia", crop: "thumb" },
-    { overlay: new cloudinary.Layer().publicId("cloudinary_icon"), gravity: "south_east", x: 5, y: 5, width: 50, opacity: 60, effect: "brightness:200" },
-    { angle: 10 }
-  ]
-}).toHtml();
+
+window.fbAsyncInit = function () {
+  FB.init({
+    appId: '371384870299199',
+    cookie: true,  // enable cookies to allow the server to access 
+    // the session
+    xfbml: true,  // parse social plugins on this page
+    version: 'v3.2' // use graph api version 2.8
+  });
+
+  // Now that we've initialized the JavaScript SDK, we call 
+  // FB.getLoginStatus().  This function gets the state of the
+  // person visiting this page and can return one of three states to
+  // the callback you provide.  They can be:
+  //
+  // 1. Logged into your app ('connected')
+  // 2. Logged into Facebook, but not your app ('not_authorized')
+  // 3. Not logged into Facebook and can't tell if they are logged into
+  //    your app or not.
+  //
+  // These three cases are handled in the callback function.
+
+  FB.getLoginStatus(function (response) {
+    statusChangeCallback(response);
+  });
+
+};
+
+// Load the SDK asynchronously
+(function (d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "https://connect.facebook.net/en_US/sdk.js";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
+
+// Here we run a very simple test of the Graph API after login is
+// successful.  See statusChangeCallback() for when this call is made.
+function testAPI() {
+  console.log('Welcome!  Fetching your information.... ');
+  FB.api('/me', function (response) {
+    console.log(JSON.stringify(response));
+    console.log('Successful login for: ' + response.name);
+    document.getElementById('status').innerHTML =
+      'Thanks for logging in, ' + response.name + '!';
+  });
+};
+//Get profile pic
+function profilePic() {
+  FB.api('/me/albums', function (response) {
+    for (album in response.data) {
+
+      // Find the Profile Picture album
+      if (response.data[album].name == "Profile Pictures") {
+
+        // Get a list of all photos in that album.
+        FB.api(response.data[album].id + "/photos", function (response) {
+
+          //The image link
+          image = response.data[0].images[0].source;
+          console.log(JSON.stringify(response.data[0].images[0].source));
+        });
+      }
+    }
+  });
+};
 
 // Firebase for Profile Data
 // Initialize Firebase
