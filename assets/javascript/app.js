d03b2ac9a18a8f7c96d53d77f67433136a253725
Mark Langhamer <langhamerm@gmail.com>
2018-11-30 15:28:02 -0600

    trim fix

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -418,7 +418,7 @@ $("#btn").on("click", function () {
   database.ref().on("value", function (snapshot) {
     console.log(snapshot.val());
     yourLocation = snapshot.child("players").child(2).val().city;
-    profilePic2 = snapshot.child("players").child(2).val().pic;
+    profilePic2 = snapshot.child("players").child(2).val().pic.trim();
       if (myLocation = yourLocation) {
         $("#photos").html(`<img src=${profilePic2}/>`);
         console.log(yourLocation);
@@ -430,7 +430,7 @@ $("#btn").on("click", function () {
   database.ref().on("value", function (snapshot) {
     console.log(snapshot.val());
     yourLocation = snapshot.child("players").child(1).val().city;
-    profilePic2 = snapshot.child("players").child(1).val().trim().pic;
+    profilePic2 = snapshot.child("players").child(1).val().pic.trim();
       if (myLocation = yourLocation) {
         $("#photos").html(`<img src=${profilePic2}/>`);
         console.log(yourLocation);
